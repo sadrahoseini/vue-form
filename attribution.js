1ccbf6e13ce8f0b45d2931608775dbe889a92af4
sadrix <s.sadra.hoseini@gmail.com>
2022-12-27 23:29:18 +0330

    fix: :bug: Fix has error bug

--- attribution.js
+++ attribution.js
@@ -1,4 +1,4 @@
-console.log(`Sadrix Vue Form 1.1.6 by @sadrix - https://sadrix.com
+console.log(`Sadrix Vue Form 1.1.7 by @sadrix - https://sadrix.com
 License - https://sadrix.com MIT
 Copyright 2023 Sadrix, Inc.
 `)
